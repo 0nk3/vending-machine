bb1d3a3a632491c41d2ebea19e7c984cc5c44a0a
0nk3 <0nk3.0nk3@gmail.com>
2020-11-04 19:02:50 +0200

    keeping track of the selected item

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -14,14 +14,17 @@ const ITEMS_DATA: Items[] = [
   {position: 1, name: 'Fizz Pop', price: 1, remaining: 5},
   {position: 2, name: 'Coke', price: 10, remaining: 5},
   {position: 3, name: 'Bar One', price: 8, remaining: 5},
-]
+  {position: 4, name: 'Fanta', price: 10, remaining: 5},
+  {position: 5, name: 'Simba', price: 7, remaining: 5},
+  {position: 6, name: 'Nik Naks', price: 7, remaining: 5},
+];
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
-  item = "Nik Naks"
+  cost = 0;
   displayedColumns: string[] = [
     "select",
     "position",
@@ -46,10 +49,19 @@ export class ItemsComponent implements OnInit {
   // Label for the checkbox on the passed row
   checkboxLabel(row?: Items): string{
     if(!row){
+      console.log("Selected ? : ", this.selection.selected);
       return `${this.itemsAllSelected()? 'select': 'deselect'} all`;
+      
     }else{
       return `${this.selection.isSelected(row)? 'deselect': 'select'} row ${row.position + 1}`;
     }
+  }
+  // Total Cost of the Selected Items
+  getTotalCost(row?: Items): number{
+    if(this.selection.isSelected(row)){
+      return this.cost+= row.price
+    }
+    return this.cost;
   }
   // constructor() { }
 
