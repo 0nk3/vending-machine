0b8764a54361cd496d5a8814a22468b45903f0ce
0nk3 <0nk3.0nk3@gmail.com>
2020-11-04 14:00:24 +0200

    add table with vending machine items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
 import { OptionsComponent } from './options/options.component';
-
+import {MatTableModule } from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemsComponent,
     OptionsComponent,
-  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,15 +1,58 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+export interface Items{
+  position: number;
+  name: string;
+  price : number;
+  remaining: number;
+  // availability: boolean; 
+}
 
+const ITEMS_DATA: Items[] = [
+  {position: 0, name: 'Jelly Beans', price: 3, remaining: 5},
+  {position: 1, name: 'Fizz Pop', price: 1, remaining: 5},
+  {position: 2, name: 'Coke', price: 10, remaining: 5},
+  {position: 3, name: 'Bar One', price: 8, remaining: 5},
+]
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
-
-  constructor() { }
+  item = "Nik Naks"
+  displayedColumns: string[] = [
+    "select",
+    "position",
+    "name",
+    "price",
+    "remaining"
+  ];
+  dataSource = new MatTableDataSource<Items>(ITEMS_DATA);
+  selection = new SelectionModel<Items>(true, []);
+  // are all items selected ?
+  itemsAllSelected(): boolean{
+    const selectedItems = this.selection.selected.length;
+    const numberOfRows = this.dataSource.data.length;
+    return selectedItems === numberOfRows;
+  }
+  // Select all rows if they are not all selected; otherwise
+  masterToggle(){
+    this.itemsAllSelected()?
+    this.selection.clear(): this.dataSource.data.forEach(
+      row => this.selection.select(row));
+  }
+  // Label for the checkbox on the passed row
+  checkboxLabel(row?: Items): string{
+    if(!row){
+      return `${this.itemsAllSelected()? 'select': 'deselect'} all`;
+    }else{
+      return `${this.selection.isSelected(row)? 'deselect': 'select'} row ${row.position + 1}`;
+    }
+  }
+  // constructor() { }
 
   ngOnInit(): void {
   }
-
 }
