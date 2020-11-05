bd9cae761d326688c6d21bfc8b2564c8a2da3c1b
0nk3 <0nk3.0nk3@gmail.com>
2020-11-05 13:06:14 +0200

    item display component complete, logic in :recycle:

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,12 +1,18 @@
+import { element } from 'protractor';
 import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+
+// Definition of an item
 export interface Items{
-  position: number;
-  name: string;
-  price : number;
-  remaining: number;
-  // availability: boolean; 
+  position?: number;
+  name?: string;
+  price? : number;
+  remaining?: number;
+}
+export interface Trasaction {
+  name?: string;
+  price? : number;
 }
 
 const ITEMS_DATA: Items[] = [
@@ -15,7 +21,7 @@ const ITEMS_DATA: Items[] = [
   {position: 2, name: 'Coke', price: 10, remaining: 5},
   {position: 3, name: 'Bar One', price: 8, remaining: 5},
   {position: 4, name: 'Fanta', price: 10, remaining: 5},
-  {position: 5, name: 'Simba', price: 7, remaining: 5},
+  {position: 5, name: 'Pop Shots', price: 7, remaining: 5},
   {position: 6, name: 'Nik Naks', price: 7, remaining: 5},
 ];
 @Component({
@@ -24,7 +30,10 @@ const ITEMS_DATA: Items[] = [
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
-  cost = 0;
+  totalCost = 0;
+  cartTotal = new Set();  
+  balance: number;
+  items: Trasaction[];
   displayedColumns: string[] = [
     "select",
     "position",
@@ -32,39 +41,41 @@ export class ItemsComponent implements OnInit {
     "price",
     "remaining"
   ];
+
   dataSource = new MatTableDataSource<Items>(ITEMS_DATA);
   selection = new SelectionModel<Items>(true, []);
   // are all items selected ?
-  itemsAllSelected(): boolean{
+  allSelectedItems(): boolean{
     const selectedItems = this.selection.selected.length;
+    this.items = this.selection.selected
+    this.totalCost = this.getTotalCost(this.items);
+    console.log("Selected:", this.selection.selected)
+    console.log("Cost    :", this.totalCost)
     const numberOfRows = this.dataSource.data.length;
+    
     return selectedItems === numberOfRows;
   }
+
   // Select all rows if they are not all selected; otherwise
   masterToggle(){
-    this.itemsAllSelected()?
+    this.allSelectedItems()?
     this.selection.clear(): this.dataSource.data.forEach(
       row => this.selection.select(row));
   }
+
   // Label for the checkbox on the passed row
   checkboxLabel(row?: Items): string{
     if(!row){
-      console.log("Selected ? : ", this.selection.selected);
-      return `${this.itemsAllSelected()? 'select': 'deselect'} all`;
-      
+      return `${this.allSelectedItems()? 'select': 'deselect'} all`;
     }else{
       return `${this.selection.isSelected(row)? 'deselect': 'select'} row ${row.position + 1}`;
     }
   }
-  // Total Cost of the Selected Items
-  getTotalCost(row?: Items): number{
-    if(this.selection.isSelected(row)){
-      return this.cost+= row.price
-    }
-    return this.cost;
-  }
-  // constructor() { }
 
+  // Total Cost of the Selected Items
+  getTotalCost(items: Trasaction[]): number{
+    return this.items.map(t => t.price).reduce((acc, value) => acc + value, 0) 
+  }
   ngOnInit(): void {
   }
 }
