776373546e6eb31ddd0a420a7dfa5c70bff46417
0nk3 <0nk3.0nk3@gmail.com>
2020-11-08 03:26:14 +0200

    selected coins retrived

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,6 +1,6 @@
 import { IProcessing } from './iprocessing';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl} from '@angular/forms';
 export interface Note{
   value: number;
   viewValue: string;
@@ -12,15 +12,8 @@ export interface Note{
   styleUrls: ['./options.component.css']
 })
 export class OptionsComponent implements IProcessing {
-  selectedNote : number;
-
-   notes: Note[] = [
-     { value: 0, viewValue: "0 cents"},
-     { value: 1, viewValue: "R1"},
-     { value: 2, viewValue: "R2"},
-     { value: 5, viewValue: "R5"}
-   ];
-
+  coins = new FormControl();
+  coinList: number[] = [0,1,2,5];
 
   getNote(): number {
     throw new Error('Method not implemented.');
@@ -34,8 +27,8 @@ export class OptionsComponent implements IProcessing {
   dispenseItems(): void {
     throw new Error('Method not implemented.');
   }
-  onNoteSelection(): number{
-    console.log("Inserted" + this.selectedNote)
-    return this.selectedNote;
+  onCoinSelection(): object{
+    // console.log("Inserted : " + this.coins.value);
+    return this.coins.value;
   }
 }
